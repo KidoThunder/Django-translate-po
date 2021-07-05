736be6ddf2e44a7b87efb4186bf0b81625f9c550
Kido <zgdisgod@hotmail.com>
2021-07-05 16:50:39 +0800

    Package: update package setup function

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="translate-po",  # Replace with your own username
-    version="1.0.13",
-    author="Erlend Eelmets",
-    author_email="erlend.eelmets@gmail.com",
+    name="Django-translate_po",
+    version="0.1.0",
+    author="Kido Zhao",
+    author_email="zgdisgod@hotmail.com",
     description="Automatic PO file translator",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/zcribe/translate-po",
-    packages=setuptools.find_packages(exclude=['docs', 'tests', 'translated', 'untranslated']),
+    url="https://github.com/KidoThunder/translate-po",
+    packages=setuptools.find_packages(exclude=['docs', 'tests']),
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 3",
@@ -22,11 +22,11 @@ setuptools.setup(
         'Topic :: Software Development :: Internationalization',
     ],
     python_requires='>=3.6',
-    keywords='po translate automatic google',
+    keywords='Django po translate automatic google & AWS',
     project_urls={
-        'Source': 'https://github.com/zcribe/translate-po',
-        'Documentation': 'https://github.com/zcribe/translate-po/docs/main.html',
-        'Author': 'http://www.erlend.ee',
+        'Source': 'https://github.com/KidoThunder/translate-po',
+        'Documentation': 'https://github.com/KidoThunder/translate-po/docs/main.html',
+        'Author': 'http://blog.zkido.cc',
     },
     entry_points={
         'console_scripts': [
@@ -35,10 +35,16 @@ setuptools.setup(
     },
     install_requires=[
         'polib>=1.1.0',
-        'googletrans>=3.0.0'
+        'googletrans>=3.0.0',
+        'django>=2.1.12',
+        'boto3>=1.17.91',
+        'redis>=3.5.3'
     ],
     setup_requires=[
         'polib>=1.1.0',
-        'googletrans>=3.0.0'
+        'googletrans>=3.0.0',
+        'django>=2.1.12',
+        'boto3>=1.17.91',
+        'redis>=3.5.3'
     ],
 )
